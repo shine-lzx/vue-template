7322a6e7f68f5800bb973f252b528d9d32204c1b
shine-lzx <1597395958@qq.com>
2020-04-02 15:24:46 +0800

    图片验证码前端生成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">首页</div>
+    <div class="verificationCode">
+      <div class="codeBackground"></div>
+      <img class="codeImage" :src="imageData" alt="verificationCode">
+      <input v-model="codeInput" type="text">
+      <div>{{ validInput }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import generateCode from '@/utils/code.js'
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      codeInput: '',
+      imageData: '',
+      codeText: ''
+    }
+  },
+  computed: {
+    validInput() {
+      return this.codeText === this.codeInput ? 'correct' : 'not correct'
+    }
+  },
+  mounted() {
+    const { code, data } = generateCode(120, 48)
+    console.log('code', code)
+    console.log('data', data)
+    this.imageData = data
+    this.codeText = code
+  }
 }
 </script>
 
