460581f279a6cdf15be1e7a4f28d35ad3b16c1e4
shine-lzx <1597395958@qq.com>
2020-04-20 13:57:48 +0800

    updata

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,31 +10,31 @@ module.exports = {
     'space-before-function-paren': [0, 'never'],
     "no-tabs": "off",
     'no-console': 'off',
-    "vue/html-self-closing": ["error", {
-      "html": {
-        "void": "never",
-        "normal": "any",
-        "component": "any"
-      },
-      "svg": "always",
-      "math": "always"
-    }],
-    "vue/max-attributes-per-line": [2, {
-      "singleline": 10,
-      "multiline": {
-        "max": 1,
-        "allowFirstLine": false
-      }
-    }],
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "vue/name-property-casing": ["error", "PascalCase"],
-    "vue/no-v-html": "off",
-    'accessor-pairs': 2,
-    'arrow-spacing': [2, {
-      'before': true,
-      'after': true
-    }],
+    // "vue/html-self-closing": ["error", {
+    //   "html": {
+    //     "void": "never",
+    //     "normal": "any",
+    //     "component": "any"
+    //   },
+    //   "svg": "always",
+    //   "math": "always"
+    // }],
+    // "vue/max-attributes-per-line": [2, {
+    //   "singleline": 10,
+    //   "multiline": {
+    //     "max": 1,
+    //     "allowFirstLine": false
+    //   }
+    // }],
+    // "vue/singleline-html-element-content-newline": "off",
+    // "vue/multiline-html-element-content-newline": "off",
+    // "vue/name-property-casing": ["error", "PascalCase"],
+    // "vue/no-v-html": "off",
+    // 'accessor-pairs': 2,
+    // 'arrow-spacing': [2, {
+    //   'before': true,
+    //   'after': true
+    // }],
   },
   parserOptions: {
     parser: 'babel-eslint'
